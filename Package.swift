eb624c904c3ae5662a6fba72552862f92403f172
Помозов Антон Владимирович <a.pomozov@tinkoff.ru>
2022-02-07 15:49:36 +0300

    Fix naming for Bazel

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "libwebp-xcode",
-            targets: ["libwebp"]),
+            targets: ["libwebp-xcode"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "libwebp",
+            name: "libwebp-xcode",
             dependencies: [],
             path: ".",
             sources: ["libwebp/src"],
