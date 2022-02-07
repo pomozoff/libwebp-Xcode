bf0811ac07a7f8a4f4404e920a35b4858e6469b8
Anton Pomozov <pomozoff@mail.ru>
2022-02-07 15:35:31 +0300

    Fix naming for Bazel

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "libwebp",
+    name: "libwebp-xcode",
     platforms: [
         .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "libwebp",
+            name: "libwebp-xcode",
             targets: ["libwebp"]),
     ],
     dependencies: [
